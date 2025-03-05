55e1b43b7a6f10821732beb907b08eb43422b121
Atharva Yemul <atharvayemul04@gmail.com>
2025-03-05 22:43:04 +0530

    Merge Strings Alternately

--- Strings/Merge_Strings_Alternately.java
+++ Strings/Merge_Strings_Alternately.java
@@ -1,3 +1,16 @@
+/* Solved Using Two Pointer Method
+
+// Learnings -->
+
+1) StringBuilder is a class which provides many methods on strings which String class do not provide
+
+2) Instead of word[i] in java --> word.charAt(i) function is used to access the index value in string.
+
+3) toString() function can be used to convert anything into the string
+
+ */
+
+
 public class Merge_Strings_Alternately {
     public static void main(String[] args) {
         String word1 = "abc";
@@ -6,16 +19,18 @@ public class Merge_Strings_Alternately {
         StringBuilder string = new StringBuilder();
 
         int i = 0 ;
+        int j = 0 ;
 
-        while (i < word1.length() || i < word2.length())
+        while (i < word1.length() || j < word2.length())
         {
             if (i < word1.length()){
                 string.append(word1.charAt(i));
+                i++;
             }
-            if (i < word2.length()){
-                string.append(word2.charAt(i));
+            if (j < word2.length()){
+                string.append(word2.charAt(j));
+                j++;
             }
-            i++;
         }
             System.out.println(string.toString());
 
